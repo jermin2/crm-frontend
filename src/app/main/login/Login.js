65cc10c36b49453dc5b111c834a68edb2485bc88
jermin2 <jermin2@gmail.com>
2021-11-14 01:36:11 +1300

    Changed login screen

--- src/app/main/login/Login.js
+++ src/app/main/login/Login.js
@@ -1,26 +1,27 @@
-import Card from '@mui/material/Card';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { styled, darken } from '@mui/material/styles';
-import CardContent from '@mui/material/CardContent';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import Typography from '@mui/material/Typography';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { submitLogin } from 'app/auth/store/loginSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Checkbox from '@mui/material/Checkbox';
+import Divider from '@mui/material/Divider';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import Auth0LoginTab from './tabs/Auth0LoginTab';
-import FirebaseLoginTab from './tabs/FirebaseLoginTab';
-import JWTLoginTab from './tabs/JWTLoginTab';
+import * as yup from 'yup';
+import _ from '@lodash';
 
 const Root = styled('div')(({ theme }) => ({
-  background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
-    theme.palette.primary.dark,
-    0.5
-  )} 100%)`,
-  color: theme.palette.primary.contrastText,
+  '& .Login3-leftSection': {},
 
-  '& .Login-leftSection': {},
-
-  '& .Login-rightSection': {
+  '& .Login3-rightSection': {
     background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
       theme.palette.primary.dark,
       0.5
@@ -29,11 +30,46 @@ const Root = styled('div')(({ theme }) => ({
   },
 }));
 
-function Login() {
-  const [selectedTab, setSelectedTab] = useState(0);
+/**
+ * Form Validation Schema
+ */
+const schema = yup.object().shape({
+  email: yup.string().email('You must enter a valid email').required('You must enter a email'),
+  password: yup
+    .string()
+    .required('Please enter your password.')
+    .min(8, 'Password is too short - should be 8 chars minimum.'),
+});
 
-  function handleTabChange(event, value) {
-    setSelectedTab(value);
+const defaultValues = {
+  email: '',
+  password: '',
+  remember: true,
+};
+
+function Login() {
+  const dispatch = useDispatch();
+  const login = useSelector(({ auth }) => auth.login);
+  const { control, formState, handleSubmit, reset, trigger, setError } = useForm({
+    mode: 'onChange',
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const { isValid, dirtyFields, errors } = formState;
+
+  useEffect(() => {
+    login.errors.forEach((error) => {
+      setError(error.type, {
+        type: 'manual',
+        message: error.message,
+      });
+    });
+  }, [login.errors, setError]);
+
+  function onSubmit(model) {
+    dispatch(submitLogin(model));
+    reset(defaultValues);
   }
 
   return (
@@ -44,7 +80,7 @@ function Login() {
         className="flex w-full max-w-400 md:max-w-3xl rounded-20 shadow-2xl overflow-hidden"
       >
         <Card
-          className="Login-leftSection flex flex-col w-full max-w-sm items-center justify-center shadow-0"
+          className="Login3-leftSection flex flex-col w-full max-w-sm items-center justify-center shadow-0"
           square
         >
           <CardContent className="flex flex-col items-center justify-center w-full py-96 max-w-320">
@@ -69,64 +105,112 @@ function Login() {
               </div>
             </motion.div>
 
-            <Tabs
-              value={selectedTab}
-              onChange={handleTabChange}
-              variant="fullWidth"
-              className="w-full mb-32"
+            <form
+              name="loginForm"
+              noValidate
+              className="flex flex-col justify-center w-full"
+              onSubmit={handleSubmit(onSubmit)}
             >
-              <Tab
-                icon={
-                  <img
-                    className="h-40 p-4 bg-black rounded-12"
-                    src="assets/images/logos/jwt.svg"
-                    alt="firebase"
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Email"
+                    autoFocus
+                    type="email"
+                    error={!!errors.email}
+                    helperText={errors?.email?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
                   />
-                }
-                className="min-w-0"
-                label="JWT"
+                )}
               />
-              <Tab
-                icon={
-                  <img className="h-40" src="assets/images/logos/firebase.svg" alt="firebase" />
-                }
-                className="min-w-0"
-                label="Firebase"
-              />
-              <Tab
-                icon={<img className="h-40" src="assets/images/logos/auth0.svg" alt="auth0" />}
-                className="min-w-0"
-                label="Auth0"
-              />
-            </Tabs>
 
-            {selectedTab === 0 && <JWTLoginTab />}
-            {selectedTab === 1 && <FirebaseLoginTab />}
-            {selectedTab === 2 && <Auth0LoginTab />}
+              <Controller
+                name="password"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Password"
+                    type="password"
+                    error={!!errors.password}
+                    helperText={errors?.password?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
+              />
+
+              <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between">
+                <Controller
+                  name="remember"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormControlLabel label="Remember Me" control={<Checkbox {...field} />} />
+                    </FormControl>
+                  )}
+                />
+
+                <Link className="font-normal" to="/pages/auth/forgot-password-2">
+                  Forgot Password?
+                </Link>
+              </div>
+
+              <Button
+                variant="contained"
+                color="primary"
+                className="w-full mx-auto mt-16"
+                aria-label="LOG IN"
+                disabled={_.isEmpty(dirtyFields) || !isValid}
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
+
+            <div className="my-24 flex items-center justify-center">
+              <Divider className="w-32" />
+              <span className="mx-8 font-semibold">OR</span>
+              <Divider className="w-32" />
+            </div>
+
+            <Button variant="outlined" color="primary" size="small" className="w-192 mb-8">
+              Log in with Google
+            </Button>
+
+            <Button variant="outlined" color="primary" size="small" className="w-192">
+              Log in with Facebook
+            </Button>
           </CardContent>
 
           <div className="flex flex-col items-center justify-center pb-32">
-            <div>
-              <span className="font-normal mr-8">Don't have an account?</span>
-              <Link className="font-normal" to="/register">
-                Register
-              </Link>
-            </div>
-            <Link className="font-normal mt-8" to="/">
-              Back to Dashboard
+            <span className="font-normal">Don't have an account?</span>
+            <Link className="font-normal" to="/pages/auth/register-3">
+              Create an account
             </Link>
           </div>
         </Card>
 
-        <div className="Login-rightSection hidden md:flex flex-1 items-center justify-center p-64">
+        <div className="Login3-rightSection flex hidden md:flex flex-1 items-center justify-center p-64">
           <div className="max-w-320">
             <motion.div
               initial={{ opacity: 0, y: 40 }}
               animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
             >
-              <Typography variant="h3" color="inherit" className="font-semibold leading-tight">
-                Welcome <br />
-                to the <br /> Church in Auckland CRM
+              <Typography
+                color="inherit"
+                className="text-32 sm:text-44 font-semibold leading-tight"
+              >
+                Church <br />
+                in <br /> Auckland <br /> CRM
               </Typography>
             </motion.div>
 
@@ -134,7 +218,7 @@ function Login() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { delay: 0.3 } }}
             >
-              <Typography variant="subtitle1" color="inherit" className="mt-32">
+              <Typography variant="subtitle1" color="inherit" className="mt-32 font-medium">
                 Powerful and professional admin template for Web Applications, CRM, CMS, Admin
                 Panels and more.
               </Typography>
